4594e613e9b2bfeef9faae6dd2f3e5b5ba722383
Akshat Singh <110585273+Singhakshat14@users.noreply.github.com>
2024-09-30 13:58:09 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -5,7 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         stack=[]
-        bracket_pair={')':'(','{':'}',''}
+        bracket_pair={')':'(','{':'}',']':'['}
+        for char in s:
+            if char in bracket_pair:
+                if char in bracket_pair:
+                    if not stack or stack.pop()
         pass
 
 

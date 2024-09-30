60b6d0ba36942d96497640b3adae61fd8ca2c7d3
Akshat Singh <110585273+Singhakshat14@users.noreply.github.com>
2024-09-30 14:02:01 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -5,7 +5,13 @@ class Solution(object):
         :rtype: int
         """
         roman_value={
-            
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
 
 
